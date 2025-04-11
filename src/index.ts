9e87e913e4cb1899fee6b9d52043421d87205481
Jakob Stieger <jakobst20@gmail.com>
2025-04-11 12:40:03 +0200

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,31 @@
-import p5 from "p5";
+import p5 from 'p5';
 
 let bunnyImage: p5.Image;
 
 const p = new p5((sketch) => {
-    sketch.setup = setup;
-    sketch.preload = preload;
-    sketch.draw = draw;
+  sketch.setup = setup;
+  sketch.preload = preload;
+  sketch.draw = draw;
 });
 
 function preload() {
-    bunnyImage = p.loadImage("https://cddataexchange.blob.core.windows.net/data-exchange/bunny.png");
+  bunnyImage = p.loadImage('https://cddataexchange.blob.core.windows.net/data-exchange/bunny.png');
 }
 
 function setup() {
-    p.createCanvas(400, 400);
+  p.createCanvas(400, 400);
 }
 
 function draw() {
-    p.background("white");
-    p.image(bunnyImage, 0, 0);
+  p.background('white');
+  p.stroke('white');
+  p.noFill();
+  p.strokeWeight(2);
 
+  for (let j = 0; j <= 5000; j += 100) {
+    for (let i = 0; i <= 500; i += 100) {
+      p.image(bunnyImage, i, j, 100, 100, i, j, 100, 100);
+      p.rect(i, j, 100, 100);
+    }
+  }
 }
